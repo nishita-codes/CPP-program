a284598e420903169c8c486725afc22295dcfe48
Nishita-codes <nishitapatidar03@gmail.com>
2024-10-13 01:09:35 +0530

    added addition

---
+++ addition/addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"the value of num1 is";
+    cin>>num1;
+
+    cout<<"the value of num2 is";
+    cin>>num2;
+
+    cout<<"the sum is"<<num1+num2;
+    return 0;
+    
+}

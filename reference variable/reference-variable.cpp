b118b1dffdee092e9af4ec0b74b20c86e2c92211
Nishita-codes <nishitapatidar03@gmail.com>
2024-10-13 13:54:54 +0530

    added reference variable

---
+++ reference variable/reference-variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x =5;
+    int & y = x;
+    cout<<"the value of x is "<<x<<endl;
+    cout<<"the value of y is "<<y<<endl;
+
+    return 0;
+}

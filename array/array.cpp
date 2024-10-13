d17f56952645f2f487f7fb31e24f066e2c337c73
Nishita-codes <nishitapatidar03@gmail.com>
2024-10-14 01:00:57 +0530

    added array

---
+++ array/array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int marks[4]={23,45,56,89};
+
+cout<<"The value of marks[0] is "<<marks[0]<<endl;
+cout<<"The value of marks[1] is "<<marks[1]<<endl;
+cout<<"The value of marks[2] is "<<marks[2]<<endl;
+cout<<"The value of marks[3] is "<<marks[3]<<endl;
+return 0;
+}

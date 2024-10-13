69df65ab467c72b3424f481e59df2dde20d17053
Nishita-codes <nishitapatidar03@gmail.com>
2024-10-13 14:03:12 +0530

    added typecasting

---
+++ typecasting/typecasting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//TYPECASTING-convert one data type into another data type
+int main(){
+    int a=45;
+    float b=45.56;
+    cout<<"the value of a is "<<(int)a<<endl;//we can write either (int)a or int(a);
+    cout<<"the value of b is "<<(int)b<<endl;
+    return 0;
+}

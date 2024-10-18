2b8a7e9d757bd48c824fe647b0708e55178274b3
Nishita-codes <nishitapatidar03@gmail.com>
2024-10-18 23:35:34 +0530

    added array of object

---
+++ array of object/array-of-object.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+    int id;
+    int salary;
+    public:
+    void setid(void){
+        salary=100000;
+        cout<<"enter the id of employee"<<endl;
+        cin>>id;
+    }
+    void getid(void){
+        cout<<"the id of this employee is" <<id<< endl;
+
+    }
+};
+int main(){
+  employee fb[4];
+  for(int i=0;i<4;i++){
+    fb[i].setid();
+    fb[i].getid();
+  }
+return 0;
+}

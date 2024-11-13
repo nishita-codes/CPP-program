a449c1e544f4a19695c01a176786a165737d0771
Nishita-codes <nishitapatidar03@gmail.com>
2024-11-13 23:30:03 +0530

    added constructor

---
+++ constructor/constructor.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class complex{
+    int a,b;
+    public:
+    complex(void);//construction declaration
+     void printnumber(){
+        cout<<"your number is "<<a<<" + "<<b<<"i"<<endl;     
+        }
+     };
+     complex::complex(void){
+        a=10;
+        b=20;
+     }
+     int main(){
+        complex c1,c2,c3;
+        c1.printnumber();
+        c2.printnumber();
+        c3.printnumber();
+      return 0;
+     }
+
+
